1f130dbaf054def8a876f40bc274040e1f9f713b
Anniina-55 <anniina.leikas55@gmail.com>
2026-02-08 20:18:36 +0200

    Implemented input field and radio buttons + theming

--- app/src/main/java/com/example/alcometer/MainActivity.kt
+++ app/src/main/java/com/example/alcometer/MainActivity.kt
@@ -1,17 +1,38 @@
 package com.example.alcometer
 
+import android.R.attr.text
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableDoubleStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.alcometer.ui.theme.AlcometerTheme
+import androidx.compose.ui.graphics.Color as color
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +41,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             AlcometerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                   Alcometer(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +51,105 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun Alcometer(modifier: Modifier = Modifier) {
+
+    // state variables for inputs and result
+    var weight by remember { mutableStateOf("")}
+    //var gender by remember { mutableStateOf("Female")}
+    val genderOptions = listOf("Female", "Male")
+    val (selectedOption, onOptionSelected) = remember { mutableStateOf(genderOptions[0]) }
+    var portionQuantity by remember { mutableStateOf("")}
+    var hoursConsumed by remember { mutableStateOf("")}
+
+    /*val litres = portionQuantity.toDouble().times(0.33)
+    val grams = litres?.times(8)?.times(4.5)
+    val burning =  (weight.toDouble()) / 10
+    val gramsLeftToBurn = grams?.minus((burning * (hoursConsumed.toDouble())))
+    var result by remember { mutableDoubleStateOf(0.0) }*/
+
+    /*if(gender == "Female") {
+        gramsLeftToBurn / (weight.toDouble() * 0.6)
+        } else {
+            gramsLeftToBurn / (weight.toDouble() * 0.7)
+        }
+    )*/
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        // horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(4.dp)
+
     )
+
+    {
+        // heading
+        Text(
+            text = "Alcometer",
+            modifier = Modifier
+                .padding(top = 32.dp)
+                .fillMaxWidth(),
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.headlineMedium,
+            color = MaterialTheme.colorScheme.primary
+        )
+        // input field for weight
+        OutlinedTextField(
+            value = weight,
+            onValueChange = { weight = it },
+            label = { Text("Your weight") },
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedBorderColor =MaterialTheme.colorScheme.primary,
+                unfocusedBorderColor = MaterialTheme.colorScheme.secondary,
+                cursorColor = MaterialTheme.colorScheme.primary,
+                focusedLabelColor = MaterialTheme.colorScheme.primary
+            ),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        )
+        // error handling for weight input
+        val weightValue = weight.toIntOrNull()
+        if (weightValue != null && weightValue < 1 ) {
+            Text(
+                text = "Please enter a valid weight",
+                color = MaterialTheme.colorScheme.error
+            )
+        }
+        // radio button for gender selection, both selections are on own row
+        // loop over gender options to render each button with correct label
+
+           genderOptions.forEach {
+               option -> Row(
+                   modifier = Modifier.padding(4.dp),
+                   verticalAlignment = Alignment.CenterVertically,
+               ) {
+                   RadioButton(
+                       selected = (option == selectedOption), // set selected state based on option
+                       onClick = { onOptionSelected(option)}  // whichever option from gender list is chosen
+                   )
+               Text (
+                   text = option // label according to selection
+               )
+           }
+
+
+       }
+
+
+
+
+
+    }
+
+
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun AlcometerPreview() {
     AlcometerTheme {
-        Greeting("Android")
+        Alcometer()
     }
 }
--- app/src/main/java/com/example/alcometer/ui/theme/Color.kt
+++ app/src/main/java/com/example/alcometer/ui/theme/Color.kt
@@ -2,10 +2,9 @@ package com.example.alcometer.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Beetroot = Color(0xFF7E0046)
+val BeetrootLight = Color(0xFFB42085)
+val Pink = Color(0xFF9B00B3)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Green = Color(0xFF00685F)
+val GreenLight = Color(0xFF108F84)
